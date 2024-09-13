96ef94500db16a40aefaa88db0935a659bf91da3
PamaliWeerasinghe <pamaliweerasinghe@gmail.com>
2024-09-13 13:25:28 +0530

    removed the port

--- private/core/config.php
+++ private/core/config.php
@@ -9,8 +9,8 @@
     define('SITENAME','surplusStays');
 
 
-define('ROOT', 'http://localhost:8080/SurplusStays/public');
-define('STYLES', 'http://localhost:8080/SurplusStays/public/assets/styles');
+define('ROOT', 'http://localhost/SurplusStays/public');
+define('STYLES', 'http://localhost/SurplusStays/public/assets/styles');
 define('ASSETS', '../../../SurplusStays/public/assets');
 
 ?>
