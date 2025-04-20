0a6540ac4d15444a438a9f0f30c4bc1cfe1eb984
ChathuraHS <chathurahemanthe@gmail.com>
2025-04-20 13:52:39 +0530

    admin new changes

---
+++ private/views/AdminEditBusiness.view.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Business</title>
+    <!-- <link rel="stylesheet" href="<?=ROOT?>/assets/styles/charity_register.css"> -->
+    <link rel="stylesheet" href="<?=STYLES?>/charity_register.css">
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBoo4pzFf80sXYMtcQUux4CWSCY9nDbvig&libraries=places"></script>
+    <script>
+        let map, marker;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: 6.927079, lng: 79.861244 },
+                zoom: 8,
+            });
+
+            google.maps.event.addListener(map, 'click', function(event) {
+                const lat = event.latLng.lat();
+                const lng = event.latLng.lng();
+                document.getElementById("latitude").value = lat;
+                document.getElementById("longitude").value = lng;
+
+                if (marker) marker.setMap(null);
+                marker = new google.maps.Marker({
+                    position: event.latLng,
+                    map: map,
+                });
+            });
+        }
+    </script>
+</head>
+<body onload="initMap()">
+<?php echo $this->view('includes/navbar')?>
+
+    <div class="container">
+        <div class="left">
+        <img src="<?=ASSETS?>/images/register_background.png" class="left-image" alt="Register image">
+            <h3>Join the SurplusStays Network</h3>
+            <p>Are you a charity organization looking to receive surplus food donations from local businesses? 
+                Register with SurplusStays to connect with donors and help feed those in need.
+            </p>
+        </div>
+        <form method="post" class="right" enctype="multipart/form-data">
+            <div class="details">
+                <div class="steps">
+                <div class="step-number"><h3>ORGANIZATION DETAILS</h3></div>
+                    
+                </div>
+                <?php if (!empty($errors)): ?>
+                    <div class="error alert">
+                        <ul>
+                            <?php foreach ($errors as $error): ?>
+                                <li><?= $error ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+
+                <h4>BUSINESS NAME :</h4>
+                <input placeholder="<?=$rows->business_name?>" value='<?=get_var('name')?>' type="text" name="name" class="input" >
+                
+                <h4>BUSINESS LOGO :</h4>
+                <div class="upload-wrapper">
+                        <label for="upload-1">
+                            <img src="<?=ASSETS?>/businessImages/<?=$rows->profile_pic?>" alt="Upload Image" class="upload-icon" id="profilePreview">
+                        </label>
+                        <input type="file" id="upload-1" name="profile_picture" style="display: none;" accept="image/*">
+                    </div>
+                <h4>BUSINESS LOCATION :</h4>
+                
+                <input type="hidden" id="latitude" name="latitude" placeholder="Latitude" readonly required><br>
+                <input type="hidden" id="longitude" name="longitude" placeholder="Longitude" readonly required><br>
+                <div id="map" style="height: 400px; width: 100%;"></div><br>
+                <h4>BUSINESS EMAIL:</h4>
+                <input placeholder="<?=$rows->email?>" value='<?=get_var('email')?>' type="text" name="email" class="input"> 
+                <h4>CONTACT NUMBER :</h4>
+                <input placeholder="<?=$rows->phoneNo?>" value='<?=get_var('phone')?>' type="text" name="phone" class="input" >
+                <h4>USERNAME :</h4>
+                <input placeholder="<?=$rows->username?>" value='<?=get_var('username')?>' type="username" name="username" class="input" >
+               
+            </div>
+            <button class="register-button" type="submit">UPDATE</button>
+</form>
+    </div>
+
+<?php echo $this->view('includes/footer')?>
+
+<!-- <script>
+    document.getElementById('upload-1').addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const img = document.createElement('img');
+            img.src = e.target.result;
+            img.alt = 'Profile Preview';
+            img.style.width = '100px';
+            img.style.height = '100px';
+            // document.getElementById('profile-pic-preview').innerHTML = img;
+            document.getElementById('profile-pic-preview').src=img.src;
+            
+        }
+        reader.readAsDataURL(file);
+    }
+});
+</script> -->
+<!-- JavaScript to Show Preview -->
+<script>
+document.getElementById('upload-1').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('profilePreview').src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+});
+</script>
+
+</body>
+</html>
---
+++ private/views/ResetPassword.view.php
@@ -0,0 +1,87 @@
+<?php require APPROOT.'/views/includes/htmlHeader.view.php'?>
+    <link rel="stylesheet" href="<?=ROOT?>/assets/styles/register.css">
+    <link rel="stylesheet" href="<?= STYLES ?>/errorAlertPopup.css">
+</head>
+<body>
+    
+<?php echo $this->view('includes/navbar_unregistered')?>
+    <div class="container">
+        <div class="left">
+        <img src="<?=ASSETS?>/images/register_background.png" class="left-image" alt="Register image">
+            <h3>Join the SurplusStays Network</h3>
+            <p>Are you a charity organization looking to receive surplus food donations from local businesses? 
+                Register with SurplusStays to connect with donors and help feed those in need.
+            </p>
+        </div>
+        
+        <div class="right">
+            <div class="details">
+                <div class="steps">
+                    <h4></h4>
+                    <div class="step-number"><h3>User Login</h3></div>
+                    <p></p>
+                </div>
+                <?php if (!empty($errors)): ?>
+                    <div class="error-alert-popup">
+                                <div class="alert-header1">
+                                <?php foreach ($errors as $error): ?>
+                                <span><?= $error ?></span>
+                            <?php endforeach; ?>
+                                    <span class="close-btn" onclick="this.parentElement.parentElement.style.display='none';">&times;</span>
+                                    <!-- <span class="alert-title">Successfull</span> -->
+
+                                   
+                                        
+                                         
+                                </div>
+                           
+                    </div>
+                    
+                <?php endif; ?>   
+                <?php if (!empty($successfull)):?>
+                    <div class="successfull-alert-popup">
+                                <div class="alert-header2">
+                                <span>
+                                    <?= $successfull ?>
+                                    </span>
+                                    <span class="close-btn" onclick="this.parentElement.parentElement.style.display='none';">&times;</span>
+                                    <!-- <span class="alert-title">Successfull</span> -->
+
+                                   
+                                        
+                                         
+                                </div>
+                           
+                    </div>
+                <?php endif; ?>
+                
+                <form method="post">
+                
+                <h4>NEW PASSWORD :</h4>
+                <input value="<?=get_var('new_password')?>" placeholder="ENTER YOUR NEW PASSWORD" type="password" class="input"  name="new_password" required>
+                <h4>RE-ENTER PASSWORD :</h4>
+                <input value="<?=get_var('renter_password')?>" placeholder="RE-ENTER YOUR PASSWORD" type="password" class="input"  name="renter_password"  required>
+                <p style="margin-top: 8%;">Doesn't have an account?<a style="text-decoration: none;" href='<?=ROOT?>/register/register'>&nbsp;Register Now </a></p>
+            </div>
+            
+            <button type="submit" class="register-button">RESET</button>
+            </form>  
+        </div>
+        
+       
+       
+    </div>
+
+<?php echo $this->view('includes/footer')?>
+<?php require APPROOT.'/views/includes/htmlFooter.view.php'?>
+<script>
+            // Auto-hide after 5 seconds
+            document.addEventListener('DOMContentLoaded', function() {
+                const alert = document.querySelector('.error alert');
+                if (alert) {
+                    setTimeout(() => {
+                        alert.style.display = 'none';
+                    }, 5000);
+                }
+            });
+        </script>
